9092d49443684a3b07c011a5392582dd98bc4d54
Jerry Chen <jcdsta@gmail.com>
2022-05-30 22:09:55 +1000

    adding solution for question 9

---
+++ 9_palindrome-number/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var num = 1245421;
+var result = IsPalindrome(num);
+
+Console.WriteLine(result);
+
+
+ static bool IsPalindrome(int x)
+{
+
+    if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+
+    int reverseNumber = 0;
+    while (x > reverseNumber)
+    {
+        Console.WriteLine("---");
+        reverseNumber = reverseNumber * 10 + x % 10;
+        Console.WriteLine(reverseNumber);
+        x /= 10;
+        Console.WriteLine(x);
+    }
+
+    return x == reverseNumber || x == reverseNumber / 10;
+
+}
