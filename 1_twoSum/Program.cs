2a6923ccc623807f11b5d2a7f6497c84cc999c52
Jerry Chen <jcdsta@gmail.com>
2022-05-30 21:59:03 +1000

    adding solutions

--- 1_twoSum/Program.cs
+++ 1_twoSum/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var nums =new  int[]{4,2,6,9,3,5,6};
+var target = 15;
+var result = Solution1(nums, target);
+Console.WriteLine(result[0]);
+Console.WriteLine(result[1]);
+
+static int[] Solution1(int[] nums, int target)
+{
+    for(int i=0; i<nums.Length; i++)
+    {
+        for(int j=i+1; j<nums.Length; j++)
+        {
+            if (nums[i] == target - nums[j])
+            {
+                return new int[] { i, j };
+            }
+        }
+    }
+    return null;
+
+
+}
